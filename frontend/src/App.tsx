0897c22768b321d8a7a3b1149e886d60f8b3a88c
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-04-18 21:00:45 +0530

    Skeleton and configs

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="container py-4 px-3 mx-auto">
+            <h1>Bootstrap</h1>
+            <button className="btn btn-primary">Primary button</button>
+        </div>
+    );
+}
+
+export default App;
