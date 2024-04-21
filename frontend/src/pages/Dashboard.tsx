1d7f31e95ff6d8338b81db7a18db34a996251124
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-04-21 23:14:58 +0530

    Rooms browser

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,18 +1,108 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { useKeycloak } from '@react-keycloak/web';
+import { useNavigate } from 'react-router-dom';
+
+const charset = '0123456789abcdefghijklmnopqrstuvwxyz';
+function randomRoomString() {
+    let s = '';
+    for(let i = 0; i < 12; i++) {
+        s += charset[Math.floor(Math.random() * charset.length)];
+    }
+    return s;
+}
 
 const Dashboard: FC = () => {
     const { keycloak } = useKeycloak();
+    const navigate = useNavigate();
+    const [recentRooms, setRecentRooms] = useState<string[]>([]);
+    const inviteCodeRef = useRef<HTMLInputElement>(null);
+
+    const newRoom = useCallback(() => {
+        const room = randomRoomString();
+        setRecentRooms(rooms => {
+            let newRooms = [...rooms];
+            newRooms.unshift(room);
+            newRooms = newRooms.slice(0, 16);
+            return newRooms;
+        });
+        navigate(`/app/room/${randomRoomString()}`);
+    }, [navigate]);
+    
+    const joinRoom = useCallback(() => {
+        if(inviteCodeRef.current && inviteCodeRef.current.value.length === 12) {
+            setRecentRooms(rooms => {
+                let newRooms = [...rooms];
+                newRooms.unshift(inviteCodeRef.current!.value);
+                newRooms = newRooms.slice(0, 16);
+                return newRooms;
+            });
+            navigate(`/app/room/${inviteCodeRef.current.value}`);
+        } else {
+            alert('Invalid invite code');
+        }
+    }, [navigate]);
+
+    useEffect(() => {
+        const storedRecentRooms = localStorage.getItem('recentRooms');
+        if (storedRecentRooms) {
+            try {
+                setRecentRooms(JSON.parse(storedRecentRooms));
+            } catch (err) {
+                console.warn(err);
+            }
+        }
+
+        return () => {
+            localStorage.setItem('recentRooms', JSON.stringify(recentRooms));
+        };
+    }, []);
 
     return (
-        <div className='flex-grow-1 p-2'>
-            <button className='btn btn-danger' onClick={() => keycloak.logout()}>Logout</button>
-            <header className='d-flex flex-column'>
-                <pre>{JSON.stringify(keycloak, null, 2)}</pre>
-            </header>
-            <main className='d-flex'>
-                <h1>Dashboard</h1>
-            </main>
+        <div>
+            <h1 className='px-4 py-2 card w-100 d-flex flex-row justify-content-between'>
+                <div>Dashboard</div>
+                <button className='btn btn-danger' onClick={() => keycloak.logout()}>Logout</button>
+            </h1>
+            <div className='container mt-5'>
+                <div className='row gap-4'>
+                    <div className='col-md-4 bg-info rounded-2'>
+                        <div className='mb-3' role='button' onClick={newRoom}>
+                            <div className='card-body'>
+                                
+                                <h5 className='card-title'>Create New Whamboard</h5>
+                                <p className='card-text'>Start with a fresh new board</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-4 bg-secondary rounded-2'>
+                        <div className='mb-3' role='button' onClick={joinRoom}>
+                            <div className='card-body'>
+                                <h5 className='card-title'>Join a Whamboard</h5>
+                                <p className='card-text'>Join your friends using a code</p>
+                            </div>
+                        </div>
+                    </div>
+                    <input ref={inviteCodeRef} placeholder='Invite code' className='col-md-3 h-50  rounded-2 border-2 px-2' />
+                </div>
+
+                <h3 className='mt-5'>Recent rooms you joined:</h3>
+                <div className='row'>
+                    {recentRooms.length > 0 && recentRooms.map((room, index) => (
+                        <div key={index} className='col-md-4'>
+                            <div
+                                className='card mb-3'
+                                style={{ cursor: 'pointer' }}
+                                onClick={() => navigate(`/app/room/${room}`)}
+                            >
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{room}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                    {!recentRooms.length && <div className='text-secondary'>Nothing here...</div>}
+                </div>
+            </div>
         </div>
     );
 };
--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -27,10 +27,10 @@ const Room = () => {
         <WSContext.Provider value={ws.current}>
             <BoardContext.Provider value={boardOptions}>
                 <div className='d-flex flex-column h-100'>
-                    <header className="p-3 bg-dark text-white">
-                        <div className="container-fluid">
-                            <div className="d-flex align-items-center justify-content-center justify-content-lg-start">
-                                <a href="/app/dashboard" className="d-flex align-items-center text-white">
+                    <header className='p-3 bg-dark text-white'>
+                        <div className='container-fluid'>
+                            <div className='d-flex align-items-center justify-content-center justify-content-lg-start'>
+                                <a href='/app/dashboard' className='d-flex align-items-center text-white'>
                                     <span>My Whamboard</span>
                                 </a>
                             </div>
@@ -38,16 +38,16 @@ const Room = () => {
                     </header>
 
                     <main className='flex-grow-1'>
-                        <div className="d-flex h-100">
-                            <div className="fixme d-flex flex-column gap-2 flex-grow-1 col-auto align-items-center justify-content-center">
+                        <div className='d-flex h-100'>
+                            <div className='fixme d-flex flex-column gap-2 flex-grow-1 col-auto align-items-center justify-content-center'>
                                 <BoardCanvas width={800} height={600} room={params.roomId} />
 
-                                <div className="d-flex justify-content-center align-items-center gap-2" style={{ width: '800px' }}>
+                                <div className='d-flex justify-content-center align-items-center gap-2' style={{ width: '800px' }}>
                                     {brushColors.map(color => (
                                         <button 
                                             key={color} 
-                                            type="button" 
-                                            className="btn btn-primary rounded-circle p-0" 
+                                            type='button' 
+                                            className='btn btn-primary rounded-circle p-0' 
                                             style={{ backgroundColor: color, width: '2rem', height: '2rem' }}
                                             onClick={() => setBoardOptions(opts => {
                                                 opts = structuredClone(opts);
@@ -61,8 +61,8 @@ const Room = () => {
                                     {brushSizes.map(size => (
                                         <button 
                                             key={size}
-                                            type="button" 
-                                            className="btn rounded-circle p-0" 
+                                            type='button' 
+                                            className='btn rounded-circle p-0' 
                                             style={{ width: '2rem', height: '2rem' }}
                                             onClick={() => setBoardOptions(opts => {
                                                 opts = structuredClone(opts);
@@ -73,21 +73,21 @@ const Room = () => {
                                             <div className='w-100 h-100 rounded-circle' style={{ backgroundColor: '#000', transform: `scale(${size / 15})`}} />
                                         </button>    
                                     ))}
-                                    {/* <button type="button" className="ms-auto btn btn-primary rounded-circle p-0" style={{ backgroundColor: '#000', width: '1rem', aspectRatio: '1' }}></button>
-                                    <button type="button" className="btn btn-primary rounded-circle p-0" style={{ backgroundColor: '#000', width: '1.25rem', aspectRatio: '1' }}></button>
-                                    <button type="button" className="btn btn-primary rounded-circle p-0" style={{ backgroundColor: '#000', width: '1.5rem', aspectRatio: '1' }}></button>
-                                    <button type="button" className="btn btn-primary rounded-circle p-0" style={{ backgroundColor: '#000', width: '1.75rem', aspectRatio: '1' }}></button>
-                                    <button type="button" className="btn btn-primary rounded-circle p-0" style={{ backgroundColor: '#000', width: '2rem', aspectRatio: '1' }}></button> */}
+                                    {/* <button type='button' className='ms-auto btn btn-primary rounded-circle p-0' style={{ backgroundColor: '#000', width: '1rem', aspectRatio: '1' }}></button>
+                                    <button type='button' className='btn btn-primary rounded-circle p-0' style={{ backgroundColor: '#000', width: '1.25rem', aspectRatio: '1' }}></button>
+                                    <button type='button' className='btn btn-primary rounded-circle p-0' style={{ backgroundColor: '#000', width: '1.5rem', aspectRatio: '1' }}></button>
+                                    <button type='button' className='btn btn-primary rounded-circle p-0' style={{ backgroundColor: '#000', width: '1.75rem', aspectRatio: '1' }}></button>
+                                    <button type='button' className='btn btn-primary rounded-circle p-0' style={{ backgroundColor: '#000', width: '2rem', aspectRatio: '1' }}></button> */}
                                 </div>
                             </div>
 
-                            <div className="border border-primary p-2" style={{ width: '24rem' }}>
+                            <div className='border border-primary p-2' style={{ width: '24rem' }}>
                                 <div className='col-9'>Chat Window</div>
                             </div>
                         </div>
                     </main>
 
-                    <footer className="p-1 bg-light">
+                    <footer className='p-1 bg-light'>
                         <button className='btn'>Export</button>
                     </footer>
                 </div>
